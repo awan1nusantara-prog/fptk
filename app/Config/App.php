845bae22e8d9d141d3d2c00df842e80761cc0ba4
awan1nusantara-prog <awan1nusantara@gmail.com>
2025-12-23 14:33:54 +0700

    add ignore

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/fptk/fptk/';
+    public string $baseURL = 'http://localhost/fptk/public/';
 
 
     /**
