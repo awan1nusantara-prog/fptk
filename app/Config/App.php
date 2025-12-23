f73dd4d218e24a9d932b230c684e57dc3fab34ed
awan1nusantara-prog <awan1nusantara@gmail.com>
2025-12-23 17:30:32 +0700

    ok

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://10.101.56.236:8082/fptk/public/';
+    public string $baseURL = 'http://10.101.56.236:8082/';
 
 
     /**
